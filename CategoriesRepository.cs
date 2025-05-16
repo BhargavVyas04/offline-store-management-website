f58484ddc26771b1a8e7b02182e4c7e2aedd7561
Bhargav Vyas <vyasb014@gmail.com>
2025-05-16 13:02:05 +0530

    Add files via upload
    ASP.NET MVC

---
+++ CategoriesRepository.cs
@@ -0,0 +1,70 @@
+namespace WebApp.Models
+{
+    public static class CategoriesRepository
+    {
+        // data that seeds in memory storage
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category { CategoryId = 1, Name = "Cans", Description = "Contains the essential nutrients your kitty needs to support a strong immune system." },
+            new Category { CategoryId = 2, Name = "Treats", Description = "Your kitty will love the crunchy outside and soft and creamy inside." },
+            new Category { CategoryId = 3, Name = "Toys", Description = "Stimulates your kitty’s natural hunting instincts to spark up play and exercise." }
+        };
+
+        public static void AddCategory(Category category)
+        {
+            if (_categories != null && _categories.Count > 0)
+            {
+                var maxId = _categories.Max(x => x.CategoryId);
+                category.CategoryId = maxId + 1;
+            }
+            else
+            {
+                category.CategoryId = 1;
+            }
+            if (_categories == null) _categories = new List<Category>();
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId); // LINQ
+
+            // return a copy, instead of the real data from the database
+            if (category != null) 
+            { 
+                return new Category
+                {
+                    CategoryId = category.CategoryId,
+                    Name = category.Name,
+                    Description = category.Description,                    
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var categoryToUpdate = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+
+    }
+}
---
+++ ProductsRepository.cs
@@ -0,0 +1,109 @@
+﻿namespace WebApp.Models
+{
+    public class ProductsRepository
+    {
+        private static List<Product> _products = new List<Product>()
+        {
+            new Product { ProductId = 1, CategoryId = 1, Name = "Applaws Mousse Tuna Grain-Free Wet Cat Food", Quantity = 100, Price = 1.99 },
+            new Product { ProductId = 2, CategoryId = 1, Name = "Applaws Chicken Breast with Pumpkin Canned Cat Food", Quantity = 200, Price = 1.99 },
+            new Product { ProductId = 3, CategoryId = 2, Name = "Temptations MixUps Surfers' Delight Flavor Soft & Crunchy Cat Treats", Quantity = 300, Price = 15.50 },
+            new Product { ProductId = 4, CategoryId = 2, Name = "Inaba Churu for Kittens Chicken Recipe Puree Grain-Free Lickable Cat Treats", Quantity = 100, Price = 3.99 },
+            new Product { ProductId = 5, CategoryId = 3, Name = "Frisco Bird with Feathers Teaser Wand Cat Toy with Catnip", Quantity = 100, Price = 4.08 },
+            new Product { ProductId = 6, CategoryId = 3, Name = "Quirky Kitty Cute Koi Wand Cat Toy", Quantity = 100, Price = 8.99 }
+        };
+
+        public static void AddProduct(Product product)
+        {
+            if (_products != null && _products.Count > 0)
+            {
+                var maxId = _products.Max(x => x.ProductId);
+                product.ProductId = maxId + 1;
+            }
+            else
+            {
+                product.ProductId = 1;
+            }
+
+            if (_products == null) _products = new List<Product>();
+            _products.Add(product);
+        }
+
+        public static List<Product> GetProducts(bool loadCategory = false) 
+        {
+            if (!loadCategory)
+            {
+                return _products;
+            }
+            else
+            {
+                if (_products != null && _products.Count > 0)
+                {
+                    _products.ForEach(p => 
+                    {
+                        if (p.CategoryId.HasValue)
+                            p.Category = CategoriesRepository.GetCategoryById(p.CategoryId.Value);
+                    });
+                }
+                return _products??new List<Product>();
+            }
+        }
+
+        public static Product? GetProductById(int productId, bool loadCategory = false)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                var prod = new Product
+                {
+                    ProductId = product.ProductId,
+                    Name = product.Name,
+                    Quantity = product.Quantity,
+                    Price = product.Price,
+                    CategoryId = product.CategoryId                    
+                };
+                if (loadCategory && prod.CategoryId.HasValue)
+                {
+                    prod.Category = CategoriesRepository.GetCategoryById(prod.CategoryId.Value);
+                }
+                return prod;
+            }
+            return null;
+        }
+
+        public static void UpdateProduct(int productId, Product product)
+        {
+            if (productId != product.ProductId) return;
+
+            var productToUpdate = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (productToUpdate != null)
+            {
+                productToUpdate.Name = product.Name;
+                productToUpdate.Quantity = product.Quantity;
+                productToUpdate.Price = product.Price;
+                productToUpdate.CategoryId = product.CategoryId;
+            }
+        }
+
+        public static void DeleteProduct(int productId)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+
+        public static List<Product> GetProductsByCategoryId(int categoryId)
+        {
+            var products = _products.Where(x => x.CategoryId == categoryId);
+            if (products != null)
+            {
+                return products.ToList();
+            }
+            else
+            {
+                return new List<Product>();
+            }
+        }
+    }
+}
